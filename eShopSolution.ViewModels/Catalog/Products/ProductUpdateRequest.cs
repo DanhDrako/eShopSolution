476bf83ef6c34edeb6e50b12a8f2099681f7f866
Danh Drako <nkocdanh58@gmail.com>
2022-09-19 03:25:54 +0700

    update database

--- eShopSolution.ViewModels/Catalog/Products/ProductUpdateRequest.cs
+++ eShopSolution.ViewModels/Catalog/Products/ProductUpdateRequest.cs
@@ -18,6 +18,8 @@ namespace eShopSolution.ViewModels.Catalog.Products
 
         public string SeoAlias { get; set; }
         public string LanguageId { set; get; }
+        public bool? IsFeatured { get; set; }
+
         public IFormFile ThumbnailImage { get; set; }
 
     }
