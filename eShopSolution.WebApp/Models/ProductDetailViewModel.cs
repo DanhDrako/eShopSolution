b3bff2445f4203ead32c5d855beab0d8b54d1fd7
Danh Drako <nkocdanh58@gmail.com>
2022-09-25 10:57:22 +0700

    show category & product details

---
+++ eShopSolution.WebApp/Models/ProductDetailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using eShopSolution.ViewModels.Catalog.Categories;
+using eShopSolution.ViewModels.Catalog.ProductImages;
+using eShopSolution.ViewModels.Catalog.Products;
+
+namespace eShopSolution.WebApp.Models
+{
+	public class ProductDetailViewModel
+	{
+        public CategoryVm Category { get; set; }
+
+        public ProductVm Product { get; set; }
+
+        public List<ProductVm> RelatedProducts { get; set; }
+
+        public List<ProductImageViewModel> ProductImages { get; set; }
+    }
+}
